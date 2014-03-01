fedcc75c1dc6dcaa8dda796953c1eb7d3d0f9bae
Iker Pedrosa <ikerpedrosam@gmail.com>
2014-03-01 19:49:04 +0100

    Show feasibility results

---
+++ src/platforms/android/assets/www/js/feasibility_results.js
@@ -0,0 +1,25 @@
+
+function drawFeasibilityResult(type, bResult, sumResult, upperResult){
+	var div = document.getElementById("feasibility_result");
+	var text;
+	
+	if(type==0){
+		if(bResult==true){
+			text = "<h3>The task set is feasible.</h3>";
+			text += "<p>"+sumResult+"<"+upperResult+"</p>";
+		}else{
+			text = "<h3>The task set shouldn't be feasible, but it could be.</h3>";
+			text += "<p>"+sumResult+">="+upperResult+"</p>";
+		}
+	}else{
+		if(bResult==true){
+			text = "<h3>The task set is feasible.</h3>";
+			text += "<p>"+sumResult+"<="+1+"</p>";
+		}else{
+			text = "<h3>The task set isn't feasible.</h3>";
+			text += "<p>"+sumResult+">"+1+"</p>";
+		}
+	}
+	
+	div.innerHTML = text;
+}
