5151b6e7efc7ac9ce76e6c06e7a1a8e46fd3271a
Iker Pedrosa <ikerpedrosam@gmail.com>
2014-03-16 18:14:40 +0100

    Show results for DMS algorithm

--- src/platforms/android/assets/www/js/results.js
+++ src/platforms/android/assets/www/js/results.js
@@ -24,3 +24,39 @@ function drawFeasibilityResult(type, bResult, sumResult, upperResult){
 	div.innerHTML = text;
 }
 
+function drawUniprocessorResult(feasible, taskNumber, period, results){
+	var div = document.getElementById("uniprocessor_results");
+	var text;
+	
+	if(feasible == true){
+		text = "<h3>The task set is feasible.</h3>";
+	}else{
+		text = "<h3>The task set isn't feasible.</h3>";
+	}
+	
+	text += drawUniprocessorCalculations(taskNumber, period, results);
+	
+	div.innerHTML = text;
+}
+
+function drawUniprocessorCalculations(taskNumber, period, results){
+	var i;
+	var text;
+	
+	text = "<table><tr><th>Task</th><th>Result</br>R <= D</th></tr>";
+	
+	for(i = 1; (i - 1) < taskNumber; i++){
+		text += "<tr>" +
+				"<td>T"+i+"</td>";
+		if(results[i] <= period[i]){
+			text += "<td>"+results[i]+" <= "+period[i]+"</td>";
+		}else{
+			text += "<td>This task is not schedulable</td>";
+		}
+		text += "</tr>";
+	}
+	
+	text += "</table>";
+	
+	return text;
+}
--- src/platforms/android/assets/www/js/uniprocessor_calculations.js
+++ src/platforms/android/assets/www/js/uniprocessor_calculations.js
@@ -85,9 +85,13 @@ function wCalculations(contTask){
 		i++;
 		w[i] = parseInt(execTime[contTask]);
 		for(j = 1 ; j < contTask; j++){
-			w[i] += parseInt(Math.ceil(w[i-1]/period[j]) * execTime[j];
+			w[i] += parseInt(Math.ceil(w[i-1]/period[j]) * execTime[j]);
 		}
-	}while(w[i] != w[i-1]);
+	}while(w[i] != w[i-1] && i < 100);
+	
+	if(i == 99){
+		w[i] = -1;
+	}
 	
 	return w[i];
 }
